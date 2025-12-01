a3495060065c8c0eb4ca0d18f45f9aa27ba6c3ae
inazekan1 <ina.zekan1@gmail.com>
2025-12-01 13:29:36 +0100

    drhdrhd

--- datoteke/Form1.Designer.cs
+++ datoteke/Form1.Designer.cs
@@ -28,13 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.txtPrezime = new System.Windows.Forms.TextBox();
+            this.txtIndeks = new System.Windows.Forms.TextBox();
+            this.btnSpremi = new System.Windows.Forms.Button();
+            this.btnPrikazi = new System.Windows.Forms.Button();
+            this.lstStudenti = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ime:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Prezime:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Broj indeksa:";
+            // 
+            // txtIme
+            // 
+            this.txtIme.Location = new System.Drawing.Point(112, 32);
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(100, 20);
+            this.txtIme.TabIndex = 3;
+            // 
+            // txtPrezime
+            // 
+            this.txtPrezime.Location = new System.Drawing.Point(112, 55);
+            this.txtPrezime.Name = "txtPrezime";
+            this.txtPrezime.Size = new System.Drawing.Size(100, 20);
+            this.txtPrezime.TabIndex = 4;
+            // 
+            // txtIndeks
+            // 
+            this.txtIndeks.Location = new System.Drawing.Point(112, 76);
+            this.txtIndeks.Name = "txtIndeks";
+            this.txtIndeks.Size = new System.Drawing.Size(100, 20);
+            this.txtIndeks.TabIndex = 5;
+            // 
+            // btnSpremi
+            // 
+            this.btnSpremi.Location = new System.Drawing.Point(41, 118);
+            this.btnSpremi.Name = "btnSpremi";
+            this.btnSpremi.Size = new System.Drawing.Size(75, 23);
+            this.btnSpremi.TabIndex = 6;
+            this.btnSpremi.Text = "Spremi";
+            this.btnSpremi.UseVisualStyleBackColor = true;
+            // 
+            // btnPrikazi
+            // 
+            this.btnPrikazi.Location = new System.Drawing.Point(137, 118);
+            this.btnPrikazi.Name = "btnPrikazi";
+            this.btnPrikazi.Size = new System.Drawing.Size(75, 23);
+            this.btnPrikazi.TabIndex = 7;
+            this.btnPrikazi.Text = "Prikaži";
+            this.btnPrikazi.UseVisualStyleBackColor = true;
+            // 
+            // lstStudenti
+            // 
+            this.lstStudenti.FormattingEnabled = true;
+            this.lstStudenti.Location = new System.Drawing.Point(41, 161);
+            this.lstStudenti.Name = "lstStudenti";
+            this.lstStudenti.Size = new System.Drawing.Size(120, 95);
+            this.lstStudenti.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstStudenti);
+            this.Controls.Add(this.btnPrikazi);
+            this.Controls.Add(this.btnSpremi);
+            this.Controls.Add(this.txtIndeks);
+            this.Controls.Add(this.txtPrezime);
+            this.Controls.Add(this.txtIme);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
-        }
+           
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
+        }
+        // Add the missing Form1_Load method to resolve the CS1061 error.
+     
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.TextBox txtPrezime;
+        private System.Windows.Forms.TextBox txtIndeks;
+        private System.Windows.Forms.Button btnSpremi;
+        private System.Windows.Forms.Button btnPrikazi;
+        private System.Windows.Forms.ListBox lstStudenti;
     }
 }
 
--- datoteke/Form1.cs
+++ datoteke/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,38 @@ namespace datoteke
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            string Linija;
+            Linija = label1.Text + "|"+ txtPrezime.Text + ","+ txtIndeks.Text;
+            StreamWriter sw = new StreamWriter("podaci.txt", true);
+            sw.WriteLine(Linija);
+            sw.Close();
+            txtIme.Text = "";
+            txtPrezime.Text = "";
+            txtIndeks.Text = "";
+            MessageBox.Show("Podaci su snimljeni");
+        }
+           
+        
+
+        private void btnPrikazi_Click(object sender, EventArgs e)
+        {
+            string Linija;
+            StreamReader sr = new StreamReader("studenti.txt");
+            Linija=sr .ReadLine();
+            List<string> podaci = new List<string>();
+            while (Linija != null)
+            {
+                Linija = Linija.Replace(",", "");
+                podaci.Add(Linija);
+                Linija=sr.ReadLine();
+
+            }
+            lstStudenti.DataSource = lstStudenti;
+            sr.Close();
         }
     }
 }
